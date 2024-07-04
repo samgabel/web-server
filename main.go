b1bc8c1bcdb9101a7b82dfbb0491722eb76f6a54
Sam Gabel <sam@gabelcloud.com>
2024-07-04 15:21:16 -0700

    CH.01-Servers/server
    --ASSIGNMENT--
    The Go standard library makes it easy to build a simple server. Your
    task is to build and run a server that binds to localhost:8080 and
    always responds with a 404 Not Found response.
    
    Signed-off-by: Sam Gabel <sam@gabelcloud.com>

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	const port = "8080"
+
+	// define the request multiplexer
+	requestMultiplexer := http.NewServeMux()
+
+	// define the http srv
+	srv := &http.Server{
+		Addr: "localhost:" + port,
+		Handler: requestMultiplexer,
+	}
+
+	// print report
+	log.Printf("Serving on port: %s\n", port)
+	// listens on address and calls serve
+	// log.Fatal will not close the program (os.Exit(1)) unless the server is shutdown or closed
+	log.Fatal(srv.ListenAndServe())
 }

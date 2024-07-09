05aa79ca8bfcb116b525ebda22c9328331b51271
Sam Gabel <sam@gabelcloud.com>
2024-07-09 14:20:06 -0700

    refactor: clean up comments
    Signed-off-by: Sam Gabel <sam@gabelcloud.com>

--- handlers.go
+++ handlers.go
@@ -6,26 +6,20 @@ import (
 )
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	// write "Content-Type" header
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	// write status code
 	w.WriteHeader(http.StatusOK)
-	// write "OK" to body
 	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	// write the number of api hits
 	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
 }
 
 func (cfg *apiConfig) handlerResetMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	// write the success of the reset
 	w.Write([]byte("Reset fileserverHits to 0"))
-	// reset the apiConfig struct fields
 	cfg.fileserverHits = 0
 }
--- main.go
+++ main.go
@@ -11,35 +11,22 @@ const (
 )
 
 func main() {
-	// define the request multiplexer
 	mux := http.NewServeMux()
 
-	// define api config instance
 	cfg := apiConfig{
 		fileserverHits: 0,
 	}
 
-	// register a STATEFUL fileserver handler
-	// implement the middleware to wrap the fileserver handler
 	mux.Handle("/app/*", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
-	// register a custom handler for readiness endpoint
-	// limit HTTP method access to 'GET' only
 	mux.HandleFunc("GET /healthz", handlerReadiness)
-	// register a custom handler for metrics endpoint
-	// limit HTTP method access to 'GET' only
 	mux.HandleFunc("GET /metrics", cfg.handlerMetrics)
-	// register a custom handler for metrics endpoint
 	mux.HandleFunc("/reset", cfg.handlerResetMetrics)
 
-	// define the http server
 	srv := &http.Server{
 		Addr:    "localhost:" + port,
 		Handler: mux,
 	}
 
-	// print report
 	log.Printf("Serving on port: %s\n", port)
-	// listens on address and calls serve
-	// log.Fatal will not close the program (os.Exit(1)) unless the server is shutdown or closed
 	log.Fatal(srv.ListenAndServe())
 }
